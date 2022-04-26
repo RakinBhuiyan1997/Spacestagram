6b0ad628a9513e3197d9492b68567381ebdc02fd
Rakin <Rakin>
2022-04-26 00:44:31 -0400

    Adding comments

--- src/Components/Apod.js
+++ src/Components/Apod.js
@@ -4,11 +4,18 @@ import LikeButton from "./LikeButton";
 import Loading from "./Loading";
 
 const Apod = () => {
+  //Created states to capture data after it is done fetching from our API
   const [content, setContent] = useState([]);
+  //Set a loading state to conditionally render all our data once it has been succesfully fetched.
+  //If it is not yet done loading, a spinning circle will appear in the middle with the title.
   const [loading, setLoading] = useState(true);
+
+  //Set states for the user to enter the start date and end date of when they want to set the images to load.
+  //The states are intially set at pre-determined dates to load initial data.
   const [startDate, setStartDate] = useState("2021-05-29");
   const [endDate, setEndDate] = useState("2021-06-10");
 
+  //The use effect here is used to fetch the data and parse it.
   useEffect(() => {
     fetch(
       `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&start_date=${startDate}&end_date=${endDate}`
@@ -17,30 +24,39 @@ const Apod = () => {
         return res.json();
       })
       .then((data) => {
+        //Until setContent finally sets the data from the fetch into the state content, loading will remain false.
+        //Once it is done, loading well then turn true and allow the data to render in the application based on conditional rendering.
         setContent(data);
         setLoading(false);
       });
   }, []);
+
+  //fetchData is a async/await function that is run one the user enters the dates they would like to see the data and will run on submit.
   const fetchData = async () => {
     const test = await fetch(
+      //Through interpolation, we enter the state of startDate and endDate in the fetch in order to acheive the requested data.
       `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&start_date=${startDate}&end_date=${endDate}`
     );
     const result = await test.json();
+    //Once the data is fetched, setContent will set the data into the variable content.
     setContent(result);
   };
 
+  //Here is the function that will run fetchData when the user clicks submit.
   const handleSubmit = async (e) => {
     e.preventDefault();
     fetchData();
   };
-  console.log(content);
+
   return (
     <>
+      {/* Here we see the conditional render based on the loading state in the return */}
       {loading ? (
         <Loading />
       ) : (
         <>
           <Header>
+            {/* This is where the user will input the date information. */}
             <Form onSubmit={handleSubmit}>
               <Label>Enter Start Date after 1995-06-16</Label>
               <Input
@@ -62,28 +78,31 @@ const Apod = () => {
             </Form>
           </Header>
           <PhotoWrapper>
-            {content.map((val) => (
-              <>
-                <Container>
-                  <Card>
-                    <CardHeader>
-                      <Image
-                        src={val.hdurl ? val.hdurl : val.url}
-                        alt="card__image"
-                        width="600"
-                      />
-                    </CardHeader>
-                    <CardBody>
-                      <h4>{val.title}</h4>
-                      <p>{val.explanation}</p>
-                    </CardBody>
-                    <CardFooter>
-                      <LikeButton />
-                    </CardFooter>
-                  </Card>
-                </Container>
-              </>
-            ))}
+            {content.map((val) => {
+              let num = Math.floor(Math.random() * 10000000);
+              return (
+                <>
+                  <Container>
+                    <Card key={num}>
+                      <CardHeader>
+                        <Image
+                          src={val.hdurl ? val.hdurl : val.url}
+                          alt="card__image"
+                          width="600"
+                        />
+                      </CardHeader>
+                      <CardBody>
+                        <h4>{val.title}</h4>
+                        <p>{val.explanation}</p>
+                      </CardBody>
+                      <CardFooter>
+                        <LikeButton />
+                      </CardFooter>
+                    </Card>
+                  </Container>
+                </>
+              );
+            })}
           </PhotoWrapper>
         </>
       )}
